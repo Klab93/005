5ee7de13f420aa7e12fbb0f19c019c84c3a592c3
Klab93 <37708074+Klab93@users.noreply.github.com>
2020-10-21 16:20:53 +0200

    Fini avant commantaires javadoc

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,14 +3,54 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        Matrice m1 = new Matrice(3,3,5);
-        System.out.println(m1);
+        if(args.length != 5 && args.length != 6){
+            throw new RuntimeException();
+        }
 
-        Matrice m2 = new Matrice(3,3,5);
-        Matrice m3 = m1.sub(m2);
+        int n1 = Integer.parseInt(args[0]);
+        int m1 = Integer.parseInt(args[1]);
+        int n2 = Integer.parseInt(args[2]);
+        int m2 = Integer.parseInt(args[3]);
+        int n  = Integer.parseInt(args[4]);
+        if(args.length == 5){
+            test(n1,m1,n2,m2,n);
+        }else{
+            int m = Integer.parseInt(args[5]);
+            test(n1,m1,n2,m2,n,m);
+        }
+    }
 
-        System.out.println(m1);
-        System.out.println(m2);
-        System.out.println(m3);
+    private static void test(int n1, int m1, int n2, int m2, int n){
+        Matrice one = new Matrice(n1,m1,n);
+        Matrice two = new Matrice(n2,m2,n);
+
+        System.out.println("The modulus is " + n);
+        test(one, two);
+    }
+
+    private static void test(int n1, int m1, int n2, int m2, int n, int m){
+        Matrice one = new Matrice(n1,m1,n);
+        Matrice two = new Matrice(n2,m2,m);
+
+        System.out.println("The first modulus is "  + n + "\n");
+        System.out.println("The second modulus is " + m + "\n");
+        test(one, two);
+    }
+
+    private static void test(Matrice one, Matrice two){
+        System.out.println("one");
+        System.out.println(one + "\n");
+
+        System.out.println("two");
+        System.out.println(two + "\n");
+
+        System.out.println("one + two");
+        System.out.println(one.add(two) + "\n");
+
+        System.out.println("one - two");
+        System.out.println(one.sub(two) + "\n");
+
+        System.out.println("one * two");
+        System.out.println(one.mult(two) + "\n");
     }
 }
--- src/com/company/Matrice.java
+++ src/com/company/Matrice.java
@@ -1,6 +1,7 @@
 package com.company;
 
 import java.util.Arrays;
+import java.util.function.BinaryOperator;
 
 public class Matrice {
     private int n;
@@ -9,21 +10,33 @@ public class Matrice {
     private int[][] matrice;
 
     private Matrice(int n, int m){
+        if (n < 0 || m < 0) {
+            throw new RuntimeException();
+        }
         this.n = n;
         this.m = m;
         matrice = new int[n][m];
-        for(int i = 0; i < n; ++i){
-            for(int j = 0;j < m; ++j){
-                matrice[i][j] = 0;
+    }
+
+    public Matrice(int[][] tabValue, int n, int m, int modulo){
+        this(n,m);
+        if (modulo <= 0) {
+            throw new RuntimeException();
+        }
+        this.mod = modulo;
+        for(int i = 0; i < Math.min(tabValue.length, n); ++i){
+            for(int j = 0; j < Math.min(tabValue[0].length, m); ++j){
+                matrice[i][j] = Math.floorMod(tabValue[i][j], this.mod);
             }
         }
     }
 
     public Matrice(int n, int m, int modulo){
-        this.n = n;
-        this.m = m;
+        this(n,m);
+        if (modulo <= 0) {
+            throw new RuntimeException();
+        }
         this.mod = modulo;
-        matrice = new int[n][m];
         for(int i = 0; i < n; ++i){
             for(int j = 0;j < m; ++j){
                 matrice[i][j] = (int) Math.round(Math.random() * modulo);
@@ -33,65 +46,42 @@ public class Matrice {
 
     @Override
     public String toString() {
-        String s = "[";
+        String s = "";
         for(int i = 0; i < n; ++i){
-            if (i != 0){
-                s += ",";
-            }
-            s += "[";
             for(int j = 0;j < m; ++j){
-                if (j != 0){
-                    s += ",";
-                }
                 s += matrice[i][j];
             }
-            s += "]";
+            s += "\n";
         }
-        s += "]";
-
         return s;
     }
 
-    /*public boolean isSameOrder(Matrice other){
-        return this.n != other.n || this.m != other.m || this.mod != other.mod;
-    }*/
-
-    public Matrice add(Matrice other){
+    public Matrice operation(Matrice other, BinaryOperator<Integer> op){
         if(this.mod != other.mod){
             throw new RuntimeException();
         }
-        Matrice res = new Matrice(this.n, this.m);
-        for(int i = 0; i < n; ++i){
-            for(int j = 0;j < m; ++j){
-                res.matrice[i][j] = (this.matrice[i][j] + other.matrice[i][j]) % this.mod;
+        int nMax = Math.max(this.n, other.n);
+        int mMax = Math.max(this.m, other.m);
+        Matrice one = new Matrice(this.matrice, nMax, mMax, this.mod);
+        Matrice two = new Matrice(other.matrice, nMax, mMax, other.mod);
+        Matrice res = new Matrice(nMax, mMax);
+        for(int i = 0; i < nMax; ++i){
+            for(int j = 0;j < mMax; ++j){
+                res.matrice[i][j] =  Math.floorMod(op.apply(one.matrice[i][j], two.matrice[i][j]),this.mod) ;
             }
         }
         return res;
+    }
+
+    public Matrice add(Matrice other){
+        return operation(other, ( i, j) -> i + j);
     }
 
     public Matrice sub(Matrice other){
-        if(isSameOrder(other)){
-            throw new RuntimeException();
-        }
-        Matrice res = new Matrice(this.n, this.m);
-        for(int i = 0; i < n; ++i){
-            for(int j = 0;j < m; ++j){
-                res.matrice[i][j] = (this.matrice[i][j] - other.matrice[i][j]) % this.mod;
-            }
-        }
-        return res;
+        return operation(other, ( i,  j) -> i - j);
     }
 
     public Matrice mult(Matrice other){
-        if(isSameOrder(other)){
-            throw new RuntimeException();
-        }
-        Matrice res = new Matrice(this.n, this.m);
-        for(int i = 0; i < n; ++i){
-            for(int j = 0;j < m; ++j){
-                res.matrice[i][j] = (this.matrice[i][j] * other.matrice[i][j]) % this.mod;
-            }
-        }
-        return res;
+        return operation(other, ( i,  j) -> i * j);
     }
 }
